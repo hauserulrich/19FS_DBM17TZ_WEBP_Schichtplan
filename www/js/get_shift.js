7b280a62800d0cb40489f7bdff070df8145b33a9
batrooxd <robert.batliner@gmail.com>
2019-05-14 10:15:30 +0200

    bug fix in get_shift

--- www/js/get_shift.js
+++ www/js/get_shift.js
@@ -7,9 +7,8 @@ function get_shift() {
 		url 		: 'db/get_shift.php',
 		dataType 	: 'json',
 		success 	: function(data) {
-			//console.log(data);
+			console.log(data);
 			$.each(data, function( index, value ) {
-				console.log(data);
 				$('#table_shift > tbody:last-child').append('<tr id="'+value.id_sp+'"><td class="td_nam">'+value.name+'</td><td class="td_des">'+value.beschreibung+'</td><td class="td_dat">'+value.datum+'</td><td class="td_ti1">'+value.zeitvon+'</td><td class="td_ti2">'+value.zeitbis+'</td></tr>')
 				$('#table_shift td').css("border-bottom", '1px solid #e7e7e7');
 			});
