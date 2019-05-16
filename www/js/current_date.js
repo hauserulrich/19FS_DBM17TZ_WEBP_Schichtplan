34b47dd6639e56c5e398588287efac42ae791bd0
sabrina-hug <48206609+sabrina-hug@users.noreply.github.com>
2019-05-16 19:21:48 +0200

    function current_date

---
+++ www/js/current_date.js
@@ -0,0 +1,15 @@
+function current_date() {
+		
+	var myDate = new Date();
+
+		var month = myDate.getMonth()+1;
+		var day = myDate.getDate();
+
+		var current_date = myDate.getFullYear() + '-' +
+    	((''+month).length<2 ? '0' : '') + month + '-' +
+    	((''+day).length<2 ? '0' : '') + day;
+
+		//alert(current_date);
+
+		document.getElementById("dat_create_shift").value = current_date;
+}
