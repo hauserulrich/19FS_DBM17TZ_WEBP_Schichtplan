937d74c9eb4fc8e77ecf6bb29d06c7c26102dc84
melanie-senn <48206601+melanie-senn@users.noreply.github.com>
2019-05-27 08:34:30 +0200

    Update current_date.js

--- www/js/current_date.js
+++ www/js/current_date.js
@@ -9,7 +9,6 @@ window.onload = function current_date() {
     	((''+month).length<2 ? '0' : '') + month + '-' +
     	((''+day).length<2 ? '0' : '') + day;
 
-
 		//alert(current_date);
 
 		document.getElementById("dat_create_shift").value = current_date;
