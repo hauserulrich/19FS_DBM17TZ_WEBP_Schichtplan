0d7f7014b1f462d979bfd11f45260652d7f20dea
melanie-senn <48206601+melanie-senn@users.noreply.github.com>
2019-06-08 09:52:26 +0200

    Update create_employee.js

--- www/js/create_employee.js
+++ www/js/create_employee.js
@@ -8,9 +8,9 @@ $(document).ready(function() {
 	//when click button call function
     $( "#btn_create_employee" ).click(function() {
 
-    	empty_messages(); // clear the message section
+    	empty_messages(); // clear the error message section
 
-    	var create_employee = $('#txb_employee_name').val(); //read user input
+    	var create_employee = $('#txb_employee_name').val(); //read user input (name of the employee)
     	$('#txb_employee_name').val(''); //empty textbox
 
     	if (create_employee.length > 2) { //check if user input > 2	    	
@@ -22,20 +22,20 @@ $(document).ready(function() {
 			    dataType 	: 'json',
 			    success 	: function(data) {
 			    	if (!data.success) {  
-			    		if (data.errors) { // if error then post error message to message section
+			    		if (data.errors) { // if error then post error message to message section, create_employee was not successful
 		    				$('#msg_error').html(data.errors);
-		    				$('#msg_error').css("background-color", "#f44336"); 
+		    				$('#msg_error').css("background-color", "#f44336"); //error message has a red background
 		    			}
 		   			}
 		   			else { // if error then post error message to message section
-		    			$('#msg_success').html(data.posted);
-		    			$('#msg_success').css("background-color", "#4CAF50");
+		    			$('#msg_success').html(data.posted); //create_employee is sucessful
+		    			$('#msg_success').css("background-color", "#4CAF50"); //message has a green background
 		    		}
 				} 
 			});
         } else {
-    		$('#msg_error').html('error: employee name must consist of at least 3 characters'); //if user input not > 2 post error message
-		    $('#msg_error').css("background-color", "#f44336"); 
+    		$('#msg_error').html('error: employee name must consist of at least 3 characters'); //if user input not > 2 post error message, create_employee was not successful
+		    $('#msg_error').css("background-color", "#f44336"); //error massage has a red background 
     	}
 	get_employees(); //refresh employee in dropdown menue	
 	});
